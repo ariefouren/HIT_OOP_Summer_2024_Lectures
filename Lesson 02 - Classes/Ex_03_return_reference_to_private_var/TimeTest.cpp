8c06a2f5971f102740fd203959666fa963a79aab
ariefouren <aporan@ono.ac.il>
2024-08-09 14:39:05 +0300

    1

--- Lesson 02 - Classes/Ex_03_return_reference_to_private_var/TimeTest.cpp
+++ Lesson 02 - Classes/Ex_03_return_reference_to_private_var/TimeTest.cpp
@@ -8,15 +8,18 @@ using namespace std;
 int main()
 {
     Time lessonStart(16, 00, 00);
+    cout <<"Initially:" << endl;
     lessonStart.print();
 
     int &hourRef = lessonStart.badSetHour(15); // get a reference to the private member hour
     hourRef = 35; // modify the private member hour through the reference
+    cout << "After hourRef = 35:" << endl;
     lessonStart.print(); // print the time
 
-    // the function retuns a not-const reference to the private member hour
+    // the function returns a not-const reference to the private member hour
     // therefore, it can be used as lvalue in an assignment
     lessonStart.badSetHour(25) = 70; 
+    cout <<"After lessonStart.badSetHour(25) = 70 :" << endl;
     lessonStart.print(); // print the time
 
     return 0;

96e50dd421be293ec96ee688f5c23dc647facd70
ariefouren <aporan@ono.ac.il>
2024-09-06 17:19:13 +0300

    06-09

--- Lesson 08. Multiple Inheritance/ex_01_multiple_inheritance/flying_car.cpp
+++ Lesson 08. Multiple Inheritance/ex_01_multiple_inheritance/flying_car.cpp
@@ -6,37 +6,34 @@
 
 using namespace std;
 
-// Base class Car
+// Base class
 class Car {
-protected:
-    string brand;
-
 public:
     Car(const string& brand) : brand(brand) {
-        cout << "A car of brand " << brand << " is created." << endl;
+        cout << toString() << " is created." << endl;
     }
 
     virtual void startEngine() {
         cout << "The car's engine started." << endl;
     }
 
-    virtual string toString() const {
-        return "Car: Brand=" + brand;
+    virtual string toString() const{
+        return "Car: [Brand=" + brand + "]";
     }
 
     ~Car() {
-        cout << "The car of brand " << brand << " is destroyed." << endl;
+        cout << toString() << " is destroyed." << endl;
     }
+
+    private:
+        string brand;
 };
 
 // Base class Airplane
 class Airplane {
-protected:
-    string model;
-
 public:
     Airplane(const string& model) : model(model) {
-        cout << "An airplane of model " << model << " is created." << endl;
+        cout << toString() << " is created." << endl;
     }
 
     virtual void takeOff() {
@@ -44,21 +41,23 @@ public:
     }
 
     virtual string toString() const {
-        return "Airplane: Model=" + model;
+        return "Airplane: [Model=" + model + "]";
     }
 
     ~Airplane() {
-        cout << "The airplane of model " << model << " is destroyed." << endl;
+        cout << toString() << " is destroyed." << endl;
     }
+
+    private: 
+        string model;
 };
 
 // Derived class FlyingCar inherits from both Car and Airplane
 class FlyingCar : public Car, public Airplane {
 public:
     FlyingCar(const string& brand, const string& model)
-        : Car(brand), Airplane(model) // Call base class constructors
-    {
-        cout << "A flying car of brand " << brand << " and model " << model << " is created." << endl;
+        : Car(brand), Airplane(model) {
+        cout << toString() << " is created." << endl;
     }
 
     // Override startEngine from Car
@@ -76,17 +75,14 @@ public:
     virtual  void takeOff() override {
         cout << "The flying car is taking off like an airplane." << endl;
     }
-
-    
+ 
     virtual string toString() const {
-        return "FlyingCar: " +
-        Car::toString() + ", " + Airplane::toString();
-    }
+        return "FlyingCar: [" + Car::toString() + ", " + 
+                                Airplane::toString() + "]";
+    } 
     
-
     ~FlyingCar() {
-        cout << "The flying car of brand " << brand << 
-        " and model " << model << " is destroyed." << endl;
+        cout << toString() << " is destroyed." << endl;
     }
 };
 

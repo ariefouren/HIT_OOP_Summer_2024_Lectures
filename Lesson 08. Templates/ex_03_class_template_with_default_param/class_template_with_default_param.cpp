2c08017664bf748c85d8a35332ba64def7e17dde
ariefouren <aporan@ono.ac.il>
2024-09-08 01:08:39 +0300

    08-09

---
+++ Lesson 08. Templates/ex_03_class_template_with_default_param/class_template_with_default_param.cpp
@@ -0,0 +1,47 @@
+// File: class_template_with_default_param.cpp
+// demonstrates class template with default type
+#define _USE_MATH_DEFINES 
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+// class template with default type 
+template<class T = double> // the default type T is double
+class Complex
+{
+public:
+	Complex(T re = 0, T im = 0) : re(re), im(im) {};
+	friend ostream& operator<<(ostream& os, const Complex& c)
+	{
+		os << "(" << c.re << (c.im >= 0 ? "+" : "") << c.im << "i)";
+		return os;
+	}
+
+private:
+	T re, im;
+};
+
+
+int main()
+{
+	int a = 1, b = 2;
+	double x = 3.1, y = 4.2;
+	Complex<int> complex1(a, b);
+	Complex<int> complex2(x, y);
+	Complex<double> complex3(a, b);
+	Complex<double> complex4(x, y);
+	Complex<> complex5(a, b);	
+	Complex<> complex6(x, y);
+	Complex<> complex7;
+
+	cout << "complex1 = " << complex1 << " \ttype: " << typeid(complex1).name() << endl;
+	cout << "complex2 = " << complex2 << " \ttype: " << typeid(complex2).name() << endl;
+	cout << "complex3 = " << complex3 << " \ttype: " << typeid(complex3).name() << endl;
+	cout << "complex4 = " << complex4 << " \ttype: " << typeid(complex4).name() << endl;
+	cout << "complex5 = " << complex5 << " \ttype: " << typeid(complex5).name() << endl;
+	cout << "complex6 = " << complex6 << " \ttype: " << typeid(complex6).name() << endl;
+	cout << "complex7 = " << complex7 << " \ttype: " << typeid(complex7).name() << endl;
+	
+}
+
